65e256b239e5dafc9abf544823d701fca6ac0116
BennySamuel <bennysamuelno.7@gmail.com>
2024-04-02 23:23:45 +0530

    setting up retrofit

--- Doodling Doods/app/src/main/java/com/game/doodlingdoods/GameApi/KtorServerApi.kt
+++ Doodling Doods/app/src/main/java/com/game/doodlingdoods/GameApi/KtorServerApi.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object KtorServerApi {
-    val serverIp ="10.52.0.90"
+    val serverIp ="10.15.217.168"
 
     val api:KtorServer by lazy {
         Retrofit.Builder()
